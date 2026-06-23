a581a34b1fb53ccb3a4d1e1388dfd3cecf6830e9
Ahmed <ahmedadilabogarin@gmail.com>
2026-06-23 15:04:47 +0300

    Initial commit

---
+++ app/src/main/java/com/vtol/zaka/HomeScreen.kt
@@ -0,0 +1,19 @@
+package com.vtol.zaka
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.vtol.zaka.ui.theme.ZakaTheme
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+
+}
+
+@Preview
+@Composable
+fun HomePreview(){
+    ZakaTheme {
+        HomeScreen()
+    }
+}
